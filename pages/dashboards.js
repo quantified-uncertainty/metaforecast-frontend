22d4d19731437399ddf3ddb6b7376ab6eeae91b2
NunoSempere <nuno.sempere@protonmail.com>
2022-02-14 14:18:44 -0500

    fix: commit changes in other folders as well

--- pages/dashboards.js
+++ pages/dashboards.js
@@ -5,11 +5,12 @@ import React from "react";
 
 // Utilities
 import { displayForecastsWrapperForSearch } from "../lib/display/displayForecastsWrappers.js";
-import displayForecasts from "../lib/display/displayForecasts.js"
+import displayForecasts from "../lib/display/displayForecasts.js";
 import Layout from "./layout.js";
 
 // Data
 import { getDashboardForecasts } from "../lib/worker/getDashboardForecasts.js";
+import { getDashboardForecastsByDashboardId } from "../lib/worker/getDashboardForecasts.js";
 
 /* get Props */
 
@@ -18,32 +19,47 @@ export async function getServerSideProps(context) {
   // so for instance if the url is metaforecasts.org/dashboards?a=b&c=d
   // this returns ({a: "b", c: "d"}})
   let dashboardId = urlQuery.dashboardId;
-
-  let dashBoardForecasts = await getDashboardForecasts({
-    ids: ["metaculus-6526", "smarkets-20206424"]
+  let props;
+  if (dashboardId) {
+    console.log(dashboardId);
+    let dashboardForecasts = await getDashboardForecastsByDashboardId({
+      dashboardId,
+    });
+    props = {
+      dashboardForecasts,
+    };
+  } else {
+    props = {
+      dashboardForecasts: [],
+    };
+  }
+  return {
+    props: props,
+  };
+  /*
+  let dashboardforecasts = await getdashboardforecasts({
+    ids: ["metaculus-6526", "smarkets-20206424"],
   });
-  let props = ({
-    dashBoardForecasts: dashBoardForecasts
-  })
+  let props = {
+    dashboardforecasts: dashboardforecasts,
+  };
 
   return {
     props: props,
   };
+	*/
 }
 
 /* Body */
-export default function Home({
-  dashBoardForecasts
-}) {
-
+export default function Home({ dashboardForecasts }) {
   return (
-    <Layout
-      key="index"
-      page={"dasbhboard"}
-    >
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {displayForecasts({ results: dashBoardForecasts, numDisplay: dashBoardForecasts.length })}
+    <Layout key="index" page={"dashboard"}>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
+        {displayForecasts({
+          results: dashboardForecasts,
+          numDisplay: dashboardForecasts.length,
+        })}
       </div>
     </Layout>
-  )
+  );
 }
