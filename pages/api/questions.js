5960c0f4289b896346d1247a72f56daa238fe611
NunoSempere <nuno.sempere@protonmail.com>
2022-03-03 13:38:59 -0500

    feat: preliminary api work

---
+++ pages/api/questions.js
@@ -0,0 +1,25 @@
+// see: https://nextjs.org/docs/api-routes/introduction
+
+import axios from "axios";
+
+async function middleware() {
+  try {
+    let response = await axios.get(
+      `https://server.loki.red/metaforecast-all-questions`
+    );
+    let data = response.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      message:
+        "Server error. You should ping @NunoSempere on Twitter, or send him an email to nuno.semperelh@protonmail.com",
+      error: error,
+    };
+  }
+}
+
+export default async function handler(req, res) {
+  let response = await middleware();
+  res.status(200).send(response);
+}
