b6485ab96e97f0ac633370c4532ef8c632100707
Vyacheslav Matyukhin <me@berekuk.ru>
2022-03-16 23:00:57 +0300

    more prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: {
-        'quri': "url('/icons/logo.svg')",
-       },
-    }
+        quri: "url('/icons/logo.svg')",
+      },
+    },
   },
   variants: {
     extend: {},
-    margin: ['responsive', 'hover'],
+    margin: ["responsive", "hover"],
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
